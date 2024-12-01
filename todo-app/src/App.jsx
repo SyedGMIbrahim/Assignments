52bfcc648e8290160ddc03257b683abe1d569888
SyedGMIbrahim <syedgibrahim@gmail.com>
2024-12-01 14:52:10 +0530

    Todo-app created

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,30 @@
+import {useState} from "react";
+
+function App() {
+	const [todos, setTodos] = useState([{
+		title: "Go to gym",
+		description: "Go to gym from 7-9",
+		completed: false
+	}, {
+		title: "Study DSA",
+		description: "Study DSA from 9-10",
+		completed: true
+	}]) 
+
+	return (
+		<div>
+			{todos.map((todo) => {
+				return <Todo title={todo.title} description={todo.description} />
+			})}
+		</div>
+	)
+}
+
+function Todo(props) {
+	return <div>
+		<h1>{props.title}</h1>
+		<h2>{props.description}</h2>
+	</div>
+}
+
+export default App
